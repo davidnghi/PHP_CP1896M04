79fe180f044cdb0863ad4f03c00945fbc0ef3b79
davidnghi <49269680+davidnghi@users.noreply.github.com>
2020-01-15 13:55:30 +0700

    cau hinh bootstrap + jquery

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-    echo 'Hello PHP';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Trang chủ Web</title>
+	<link rel="stylesheet" href="./PHP_CP1896M04/assets/vendor/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" href="./PHP_CP1896M04/assets/vendor/font-awesome.css">
+</head>
+<body>
+	<a href=""><i class="fa fa-pencil"></i></a>
+	<!-- jquery and bootstrap.js -->
+	<script type="text/javascript" src="./PHP_CP1896M04/assets/vendor/bootstrap/js/bootstrap.js"></script>
+ 	<script type="text/javascript" src="./PHP_CP1896M04/assets/vendor/jquery/jquery-3.4.1.min.js"></script>
+ 	<script type="text/javascript" src="./PHP_CP1896M04/assets/vendor/jquery/tether.min.js"></script>
+</body>
+</html>
